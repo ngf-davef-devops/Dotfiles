32e9042cd155762f93ae762b2035b1fcb8e95dce
Francisco Sicardi <francisco.sicardi@despegar.com>
2019-10-25 17:48:44 -0300

    improve script

--- update_local_config.sh
+++ update_local_config.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-ln -s ~/Dotfiles/.bashrc ~/
-ln -s ~/Dotfiles/.bash_env ~/
-ln -s ~/Dotfiles/.bash_aliases ~/
-ln -s ~/Dotfiles/.bash_logout ~/
-ln -s ~/Dotfiles/.inputrc ~/
-ln -s ~/Dotfiles/.profile ~/
-ln -s ~/Dotfiles/.vimrc ~/
-ln -s ~/Dotfiles/.vim ~/
+
+while read filename; do
+    ln -sf ~/Dotfiles/"$filename" ~/"$filename"
+done < filelist.txt
