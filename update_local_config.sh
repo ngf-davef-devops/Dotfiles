392b2805e0a82b1ebf51d1354cf7b2c1824c187c
Francisco Sicardi Riobó <francisco.sicardi@despegar.com>
2019-10-25 15:12:26 -0300

    create script to copy config files to home directory

---
+++ update_local_config.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+ln -s ~/Dotfiles/.bashrc ~/
+ln -s ~/Dotfiles/.bash_env ~/
+ln -s ~/Dotfiles/.bash_aliases ~/
+ln -s ~/Dotfiles/.bash_logout ~/
+ln -s ~/Dotfiles/.inputrc ~/
+ln -s ~/Dotfiles/.profile ~/
+ln -s ~/Dotfiles/.vimrc ~/
+ln -s ~/Dotfiles/.vim ~/
