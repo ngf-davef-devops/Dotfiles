0e7654c4db31c41f33e63fea3c7e3a5a7981a159
Dave Fisher <fishd72@users.noreply.github.com>
2024-03-20 13:43:07 +0000

    style: Reformatted with correct indentation.

--- config/nvim/lua/plugins/which-key.lua
+++ config/nvim/lua/plugins/which-key.lua
@@ -1,14 +1,14 @@
 return {
   "folke/which-key.nvim",
-	config = function()
-	require("which-key").setup({
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
-  opts = {
-  },
-})
+  config = function()
+    require("which-key").setup({
+      event = "VeryLazy",
+      init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+      end,
+      opts = {
+      },
+    })
   end,
 }
